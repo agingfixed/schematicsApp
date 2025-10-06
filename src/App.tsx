bd0c4100616a38cbc1c6077a7c9c00bd86a5f8b5
IntrepidGains <Michaelnmcclain@gmail.com>
2025-10-06 13:04:48 -0400

    Refine draw menu layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React, { useMemo, useRef, useState } from 'react';
 import { Canvas, CanvasHandle } from './components/Canvas';
 import { Toolbar } from './components/Toolbar';
 import { MiniMap } from './components/MiniMap';
-import { DrawMenu } from './components/DrawMenu';
 import {
   selectScene,
   selectShowMiniMap,
+  selectTool,
   selectTransform,
   useSceneStore
 } from './state/sceneStore';
+import { DrawMenu } from './components/DrawMenu';
 import { BoardControls } from './components/BoardControls';
 import './App.css';
 
@@ -16,6 +17,7 @@ export const App: React.FC = () => {
   const canvasRef = useRef<CanvasHandle>(null);
   const [viewport, setViewport] = useState<{ width: number; height: number }>({ width: 0, height: 0 });
   const showMiniMap = useSceneStore(selectShowMiniMap);
+  const tool = useSceneStore(selectTool);
   const transform = useSceneStore(selectTransform);
   const scene = useSceneStore(selectScene);
 
@@ -36,10 +38,12 @@ export const App: React.FC = () => {
       <BoardControls />
       <Toolbar canvasRef={canvasRef} />
       <div className="workspace">
-        <div className="workspace__sidebar">
-          <DrawMenu />
-        </div>
         <div className="workspace__main">
+          {tool === 'draw' && (
+            <div className="workspace__draw-menu">
+              <DrawMenu />
+            </div>
+          )}
           <Canvas ref={canvasRef} onViewportChange={setViewport} />
           <div className="workspace__status">{statusText}</div>
           {showMiniMap && (
